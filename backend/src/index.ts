a37541364c096a1bcaeec46d1aac7f3cd7869779
VineethReddy <eternalbasic1@gmail.com>
2024-07-27 15:16:47 +0530

    working on sign up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.post("/api/v1/user/signup", (c) => {
-  return c.text("Hello Hono!");
+app.post("/api/v1/user/signup", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  console.log("CHECK BABY", body);
+  // TODO: Zod validation & password should be hashed
+
+  try {
+    await prisma.user.create({
+      data: {
+        name: body.name,
+        password: body.password,
+        email: body.email,
+      },
+    });
+    return c.text("User created successfully");
+  } catch (error) {
+    console.log(error);
+    c.status(411);
+    return c.text(`Invalid, Error sign up ${error}`);
+  }
 });
 app.post("/api/v1/user/signin", (c) => {
   return c.text("Hello Hono!");
