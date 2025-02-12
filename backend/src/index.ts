badfad635a545ea608d0aeee4af160791e0747c7
VineethReddy <eternalbasic1@gmail.com>
2025-02-12 20:59:18 +0530

    worked on signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
@@ -13,24 +15,26 @@ app.post("/api/v1/user/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  console.log("CHECK BABY", body);
+
   // TODO: Zod validation & password should be hashed
 
   try {
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         name: body.name,
         password: body.password,
         email: body.email,
       },
     });
-    return c.text("User created successfully");
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
   } catch (error) {
     console.log(error);
     c.status(411);
     return c.text(`Invalid, Error sign up ${error}`);
   }
 });
+
 app.post("/api/v1/user/signin", (c) => {
   return c.text("Hello Hono!");
 });
