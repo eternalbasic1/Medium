97fd3c6bd06e158c9dba677d480af376bd198434
VineethReddy <eternalbasic1@gmail.com>
2025-02-18 18:27:04 +0530

    changed Quoate

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -2,11 +2,11 @@ export const Quote = () => {
   return (
     <div className="hidden md:flex bg-slate-200 h-screen flex-col  justify-center p-8">
       <div className="font-bold text-2xl mb-4">
-        "The Customer Service I received was exceptional. The Support team went
-        above and beyond to address my concerns."
+        "The support I received was truly remarkable. The team was proactive and
+        thorough in addressing my concerns."
       </div>
-      <div className="font-semibold">Jules Winnfield</div>
-      <div className="text-slate-400">CEO, Acme Inc</div>
+      <div className="font-semibold">— Vincent Carter</div>
+      <div className="text-slate-400">CEO, Apex Solutions</div>
     </div>
   );
 };
